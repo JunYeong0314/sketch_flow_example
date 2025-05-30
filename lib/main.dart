fdda759abb95a3c286ff48b4d61e6a65552d5c1d
junyeong0314 <kkmtk00314@naver.com>
2025-05-30 16:05:15 +0900

    color change

--- lib/main.dart
+++ lib/main.dart
@@ -31,80 +31,85 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.deepPurple,
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text("SketchFlow Example App"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          spacing: 4,
-          children: [
-            _baseButton(
-              context: context,
-              routeWidget: BaseExamplePage(),
-              text: "Base Example",
-            ),
-            _baseButton(
-              context: context,
-              routeWidget: OverlayWidgetExamplePage(
-                overlayWidget: Image.asset('assets/images/example.jpg'),
-              ),
-              text: "Overlay Widget Example",
-            ),
-            _baseButton(
-              context: context,
-              routeWidget: MemoExamplePage(),
-              text: "Memo Example",
-            ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: Container(
-        height: 80,
-        color: Colors.deepPurple.withValues(alpha: 0.8),
-        padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-        child: SingleChildScrollView(
+      body: Container(
+        color: Colors.white,
+        child: Center(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            spacing: 4,
             children: [
-              Text(
-                "[Github]",
-                style: TextStyle(
-                  fontSize: 10,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
+              _baseButton(
+                context: context,
+                routeWidget: BaseExamplePage(),
+                text: "Base Example",
               ),
-              SelectableText(
-                "https://github.com/Fasoo-DigitalPage/sketch_flow",
-                style: TextStyle(
-                  fontSize: 10,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+              _baseButton(
+                context: context,
+                routeWidget: OverlayWidgetExamplePage(
+                  overlayWidget: Image.asset('assets/images/example.jpg'),
                 ),
+                text: "Overlay Widget Example",
               ),
-              SizedBox(height: 8),
-              Text(
-                "[pub.dev]",
-                style: TextStyle(
-                  fontSize: 10,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-              SelectableText(
-                "https://pub.dev/packages/sketch_flow",
-                style: TextStyle(
-                  fontSize: 10,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
+              _baseButton(
+                context: context,
+                routeWidget: MemoExamplePage(),
+                text: "Memo Example",
               ),
             ],
           ),
         ),
+      ),
+      bottomNavigationBar: SafeArea(
+          child: Container(
+            height: 80,
+            color: Colors.deepPurple.withValues(alpha: 0.8),
+            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "[Github]",
+                    style: TextStyle(
+                      fontSize: 10,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SelectableText(
+                    "https://github.com/Fasoo-DigitalPage/sketch_flow",
+                    style: TextStyle(
+                      fontSize: 10,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    "[pub.dev]",
+                    style: TextStyle(
+                      fontSize: 10,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SelectableText(
+                    "https://pub.dev/packages/sketch_flow",
+                    style: TextStyle(
+                      fontSize: 10,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
       ),
     );
   }
